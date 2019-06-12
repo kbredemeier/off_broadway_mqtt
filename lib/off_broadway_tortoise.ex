e12d84da018997654f6ecff7f975b636368d6928
Kristopher Bredemeier <kristopher.bredemeier@grandcentrix.net>
2019-06-12 17:56:37 +0200

    Adds some utility functions and type defenitions

--- lib/off_broadway_tortoise.ex
+++ lib/off_broadway_tortoise.ex
@@ -1,18 +1,95 @@
 defmodule OffBroadwayTortoise do
   @moduledoc """
-  Documentation for OffBroadwayTortoise.
+  Namespace for the broadway OffBroadwayTortoise produce.
   """
+
+  @type topic :: binary
+  @type qos :: 0 | 1 | 2
+  @type subscriptions :: [{topic, qos}]
+
+  @type conn_opt ::
+          {:host, charlist}
+          | {:port, non_neg_integer}
+          | {atom, any}
+
+  @type conn_opts :: [conn_opt, ...]
+
+  @type conn :: {:tcp | :ssl, conn_opts}
+
+  @type config :: %{
+          client_id_prefix: String.t(),
+          conn: conn
+        }
+
+  @default_transport :tcp
+  @default_host 'localhost'
+  @default_port 1883
+  @default_client_id_prefix "off_broadway_tortoise"
 
   @doc """
-  Hello world.
+  Utility function to build a for the running application unique client id that
+  can be used when connecting with the broker.
 
-  ## Examples
-
-      iex> OffBroadwayTortoise.hello()
-      :world
-
+  This ensures that multiple clients from the same application don't kick each
+  other from the broker in case the broker does not allow multiple connections
+  with the same clent id.
   """
-  def hello do
-    :world
+  @spec unique_client_id(nil | config) :: String.t()
+  def unique_client_id(config \\ nil) do
+    config = config || config()
+    random = [:positive] |> System.unique_integer() |> to_string
+    config.client_id_prefix <> "_" <> random
   end
+
+  @doc """
+  Returns the name for queue belonging to the given topic.
+  """
+  @spec queue_name(atom, topic) :: {:via, Registry, {atom, topic}}
+  def queue_name(
+        registry \\ OffBroadwayTortoise.QueueRegistry,
+        topic
+      )
+      when is_binary(topic) and is_atom(registry) do
+    {:via, Registry, {registry, topic}}
+  end
+
+  @doc """
+  Returns the runtime configuration for OffBroadwayTortoise.
+  """
+  @spec config(nil | keyword) :: config
+  def config(opts \\ nil) do
+    config = opts || Application.get_all_env(:off_broadway_tortoise)
+    client_id_prefix = config[:client_id_prefix] || @default_client_id_prefix
+
+    {transport, conn_opts} =
+      config
+      |> Keyword.get(:connection, [])
+      |> case do
+        conn_opts when is_list(conn_opts) -> conn_opts
+        _ -> []
+      end
+      |> Keyword.update(:host, @default_host, &parse_host/1)
+      |> Keyword.update(:port, @default_port, &parse_port/1)
+      |> Keyword.pop(:transport, @default_transport)
+
+    %{
+      client_id_prefix: client_id_prefix,
+      conn: {transport, conn_opts}
+    }
+  end
+
+  defp parse_host(host) when is_binary(host), do: String.to_charlist(host)
+  defp parse_host(host), do: host
+
+  defp parse_port(port) when is_integer(port), do: port
+
+  defp parse_port(port) when is_binary(port) do
+    case Integer.parse(port) do
+      {port, ""} -> port
+      _ -> raise "invalid port configured for #{__MODULE__}: #{port}"
+    end
+  end
+
+  defp parse_port(port),
+    do: raise("invalid port configured for #{__MODULE__}: #{port}")
 end
--- test/off_broadway_tortoise_test.exs
+++ test/off_broadway_tortoise_test.exs
@@ -1,8 +1,63 @@
 defmodule OffBroadwayTortoiseTest do
-  use ExUnit.Case
-  doctest OffBroadwayTortoise
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert OffBroadwayTortoise.hello() == :world
+  describe "unique_client_id_/1" do
+    test "returns a string" do
+      assert id = OffBroadwayTortoise.unique_client_id()
+      assert is_binary(id)
+      assert Regex.match?(~r/^off_broadway_tortoise_.+/, id)
+    end
+
+    test "returns unique client ids" do
+      ids =
+        for _ <- 1..100 do
+          OffBroadwayTortoise.unique_client_id()
+        end
+
+      assert ids == Enum.uniq(ids)
+    end
+
+    test "takes the client_id_prefix from config" do
+      assert id =
+               OffBroadwayTortoise.unique_client_id(%{client_id_prefix: "asd"})
+
+      assert Regex.match?(~r/^asd_.+/, id)
+    end
+  end
+
+  describe "queue_name" do
+    test "returns the process name for a topic queue server" do
+      assert {:via, Registry, {:foo, "bar"}} ==
+               OffBroadwayTortoise.queue_name(:foo, "bar")
+    end
+
+    test "returns the process name with the default registry" do
+      assert {:via, Registry, {OffBroadwayTortoise.QueueRegistry, "bar"}} ==
+               OffBroadwayTortoise.queue_name("bar")
+    end
+  end
+
+  describe "config/1" do
+    test "returns the mqtt config" do
+      assert %{
+               client_id_prefix: "off_broadway_tortoise",
+               conn: {:tcp, [host: 'vernemq', port: 1883]}
+             } == OffBroadwayTortoise.config()
+    end
+
+    test "sets transport" do
+      assert %{
+               conn: {:ssl, _}
+             } = OffBroadwayTortoise.config(connection: [transport: :ssl])
+    end
+
+    test "parses port and host" do
+      assert %{
+               conn: {:tcp, [host: 'test', port: 1111]}
+             } =
+               OffBroadwayTortoise.config(
+                 connection: [host: "test", port: "1111"]
+               )
+    end
   end
 end

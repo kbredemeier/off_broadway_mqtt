9c20cea14267914ae75fa740489256adc30f5a23
Kristopher Bredemeier <kristopher.bredemeier@grandcentrix.net>
2019-06-12 16:55:21 +0200

    Starts a supervisor and a registry with the application

--- lib/off_broadway_tortoise/application.ex
+++ lib/off_broadway_tortoise/application.ex
@@ -1,19 +1,15 @@
 defmodule OffBroadwayTortoise.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: OffBroadwayTortoise.Worker.start_link(arg)
-      # {OffBroadwayTortoise.Worker, arg}
+      {Registry, [keys: :unique, name: OffBroadwayTortoise.QueueRegistry]},
+      {DynamicSupervisor,
+       [strategy: :one_for_one, name: OffBroadwayTortoise.QueueSupervisor]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: OffBroadwayTortoise.Supervisor]
     Supervisor.start_link(children, opts)
   end

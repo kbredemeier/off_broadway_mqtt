b3abe0bc566cebf9d5d4a0b53e6ac45ef7754652
Kristopher Bredemeier <kristopher.bredemeier@grandcentrix.net>
2019-06-15 19:17:17 +0200

    Extracts assertions module

--- test/support/off_broadway_tortoise_case.ex
+++ test/support/off_broadway_tortoise_case.ex
@@ -7,13 +7,16 @@ defmodule OffBroadway.MQTTProducerCase do
 
   use ExUnit.CaseTemplate
 
+  alias OffBroadway.MQTTProducer
   alias OffBroadway.MQTTProducer.Queue
   alias OffBroadway.MQTTProducer.Config
 
   using _opts do
     quote do
-      import OffBroadway.MQTTProducerCase
+      import OffBroadway.MQTTProducer.Assertions
       import OffBroadway.MQTTProducer.Factory
+      import OffBroadway.MQTTProducerCase
+
       alias OffBroadway.MQTTProducer.Data
       alias OffBroadway.MQTTProducer.Queue
     end
@@ -85,8 +88,14 @@ defmodule OffBroadway.MQTTProducerCase do
   to the context.
   """
   def start_mqtt_client(context) do
-    client_id = OffBroadway.MQTTProducer.unique_client_id()
-    subscriptions = Map.get(context, :subscriptions) || []
+    {client_id, subscriptions} =
+      context
+      |> Map.get(:start_mqtt_client, [])
+      |> case do
+        [] -> {MQTTProducer.unique_client_id(), []}
+        {client_id, subscriptions} -> {client_id, subscriptions}
+        client_id -> {client_id, []}
+      end
 
     server_opts =
       :off_broadway_mqtt_producer
@@ -167,124 +176,7 @@ defmodule OffBroadway.MQTTProducerCase do
     Config.new(:default, queue_registry: reg, queue_supervisor: sup)
   end
 
-  @doc """
-  Tests that there is a `Tortoise.Connection` for the given `client_id`
-  registered with the `Tortoise.Registry`.
-
-  Note that this does not necessarily mean that the client is connected!
-  """
-  def assert_mqtt_client_running(client_id) do
-    Tortoise.Registry
-    |> Registry.lookup({Tortoise.Connection, client_id})
-    |> case do
-      [] ->
-        raise ExUnit.AssertionError,
-          message:
-            "Expected a Tortoise.Connection running for " <>
-              "#{inspect(client_id)} but there isn't any."
-
-      [_] ->
-        true
-    end
+  def config_from_context(_) do
+    Config.new(:default)
   end
-
-  @doc """
-  Tests that there is no `Tortoise.Connection` for the given `client_id`
-  registered with the `Tortoise.Registry`.
-  """
-  def refute_mqtt_client_running(client_id) do
-    Tortoise.Registry
-    |> Registry.lookup({Tortoise.Connection, client_id})
-    |> case do
-      [_] ->
-        raise ExUnit.AssertionError,
-          message:
-            "Expected NO Tortoise.Connection running for " <>
-              "#{inspect(client_id)} but there is one."
-
-      [] ->
-        true
-    end
-  end
-
-  @doc """
-  When sarting the mqtt client or producer you can pass `sub_ack: self()` with
-  the options to receive a message from the handler as soon as the subscription
-  is confirmed. The message received has the form
-  `{:sunscription, client_id, topic, status}`.
-  """
-  defmacro assert_receive_sub_ack(client_id, topic) do
-    do_assert_receive_sub_ack(client_id, topic)
-  end
-
-  defp do_assert_receive_sub_ack(client_id, topic) do
-    client_id_expr = maybe_pin(client_id)
-    topic_expr = maybe_pin(topic)
-
-    client_id_str = inspect_arg(client_id)
-    topic_str = inspect_arg(topic)
-
-    quote do
-      receive do
-        {:subscription, unquote(client_id_expr), unquote(topic_expr), :up} ->
-          true
-      after
-        2000 ->
-          acks =
-            collect_messages()
-            |> Enum.filter(fn
-              {:subscription, _, _, _} -> true
-              _ -> false
-            end)
-            |> Enum.map(fn {_, client_id, topic, status} ->
-              "- #{inspect(status)} for #{inspect(client_id)} on " <>
-                " #{inspect(topic)}"
-            end)
-            |> case do
-              [] -> "none"
-              acks -> Enum.join(acks, "\n")
-            end
-
-          raise ExUnit.AssertionError,
-            message:
-              "Expected the Tortoise Handler to acknowledge it's " <>
-                "subscription for #{unquote(client_id_str)} client_id to " <>
-                "#{unquote(topic_str)} topic but it didn't.\n\n" <>
-                "The following subsriptions have been acknowledged:\n\n" <>
-                "#{acks}\n"
-      end
-    end
-  end
-
-  def collect_messages(acc \\ [], action \\ :cont)
-  def collect_messages(acc, :halt), do: acc
-
-  def collect_messages(acc, :cont) do
-    receive do
-      msg ->
-        collect_messages([msg | acc], :cont)
-    after
-      100 -> collect_messages(acc, :halt)
-    end
-  end
-
-  defp maybe_pin({:_, _, _} = expr), do: expr
-
-  defp maybe_pin({_, _, _} = expr) do
-    quote do
-      ^unquote(expr)
-    end
-  end
-
-  defp maybe_pin(expr), do: expr
-
-  defp inspect_arg({:_, _, _}), do: "any"
-
-  defp inspect_arg({_, _, _} = arg) do
-    quote do
-      inspect(unquote(arg))
-    end
-  end
-
-  defp inspect_arg(arg), do: inspect(arg)
 end

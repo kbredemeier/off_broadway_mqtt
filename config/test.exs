157440cc333113322f53508d70c727f53abe2a9f
Kristopher Bredemeier <kristopher.bredemeier@grandcentrix.net>
2019-06-15 18:02:08 +0200

    Configures short dequeue_interval for test env

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :off_broadway_mqtt_producer,
   client_id_prefix: "off_broadway_mqtt_producer",
+  dequeue_interval: 100,
   server_opts: [
     host: "vernemq",
     port: 1883
